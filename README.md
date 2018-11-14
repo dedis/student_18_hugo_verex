d4272941393caae4466a5f5c93a9051de117da92
Hugo Roussel <hugo.roussel@epfl.ch>
2018-11-14 16:04:57 +0100

    modification of minimumToken scala source code to check for overflow. General readme update

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Verex
 
-Adding smart contracts to Byzcoin using verifiable Scala.
+## Overview
+
+Verex is an experimental library to deploy solidity smart contracts on the byzcoin ledger.
+
+
+## Installation
+
+```
+$ git clone https://github.com/dedis/student_18_hugo_verex
+//...
+$ cd student_18_hugo_verex/byzcoin
+$ go build && go test
+```
+
+
+
+## Formal verification of smart contracts
+
+It is possible to verify formally smart contract using the [Stainless](https://github.com/epfl-lara/stainless)  library.
+For the moment only the MinimumToken is usable. Further integration with the library is planned in the future.
+
+  
+
+ 
 
 
 
--- contracts/minimumToken/MinimumToken.sol
+++ contracts/minimumToken/MinimumToken.sol
@@ -1,36 +1,31 @@
 pragma solidity ^0.4.24;
 
-//No overflow check! Check 0xc20cC13f6efe457b515eAab43EAE2cdD6B28583a on rinkeby network
 
 contract MinimumToken {
     // Fields
-    mapping(address => uint32) balanceOf;
-    uint32 total;
+    mapping(address => uint256) balanceOf;
+    uint256 total;
     address[] participants;
 
     // Enumerations
 
     // Constructor
     constructor () public {
-      balanceOf[msg.sender] = 4294967295;
-      balanceOf[0xe745E7ceA88A02a1Fabd4aE591371eF50BFDc099] = 1;
     }
 
     // Public functions
-    function transferFrom (address from, address to, uint32 amount) public {
+    function transferFrom (address from, address to, uint256 amount) public {
         require(!(to == address(0)), "error");
         require(!(from == to), "error");
         require(amount <= balanceOf[from], "error");
+        require(balanceOf[to] <= amount + balanceOf[to], "error");
 
         balanceOf[from] = balanceOf[from] - amount;
         balanceOf[to] = balanceOf[to] + amount;
 
     }
 
-    function getBalance(address _account) public view returns (uint32){
-      return balanceOf[_account];
-    }
-
     // Private functions
 
 }
+
